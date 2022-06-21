cfb3abcbaba291d96f3872ca6537e1070052d006
ptrck <blkangl84@gmail.com>
2022-06-21 18:55:16 -0400

    added imports to main and downloaded packages for project

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score
+from sklearn import svm
+
