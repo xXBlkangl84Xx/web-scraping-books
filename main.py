1575c82201c41f30392020b8c9d4f91995429882
ptrck <blkangl84@gmail.com>
2022-06-21 18:56:22 -0400

    message

--- main.py
+++ main.py
@@ -8,3 +8,32 @@ from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
 from sklearn import svm
 
+import warnings
+warnings.filterwarnings("ignore")
+
+from sklearn.cluster import KMeans
+import re
+
+import csv as csv
+from datetime import datetime
+from matplotlib import pyplot
+from sklearn.model_selection import cross_val_score,KFold
+from sklearn.metrics import mean_absolute_error
+import matplotlib.pyplot as plt
+from scipy import stats
+
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+from sklearn.preprocessing import LabelEncoder
+
+import re
+import string
+import gensim
+
+import nltk
+from nltk.corpus import stopwords
+nltk.download('punkt')
+nltk.download('stopwords')
+
+import requests
+from bs4 import BeautifulSoup
